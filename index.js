06677fa1d3ddbb85d972aa351e16b9366bb215ad
Ben Rubin <benjaminrubin@Benjamins-MacBook-Pro.local>
2017-09-22 22:14:52 -0500

    add documentation

--- index.js
+++ index.js
@@ -5,5 +5,12 @@ if (process.env.AWS_SECRET) config.secretAccessKey = process.env.AWS_SECRET;
 if (process.env.AWS_REGION) config.region = process.env.AWS_REGION;
 if (process.env.SQS_ENDPOINT) config.endpoint = process.env.SQS_ENDPOINT;
 if (Object.keys(config).length) AWS.config.update(config);
+const events = require('./lib/events');
 
-module.exports = require('./lib/events');
+module.exports = {
+  SQS: require('./lib/sqs'),
+  SNS: require('./lib/sns'),
+  subscribe: events.subscribe,
+  publish: events.publish,
+  poll: events.poll
+};
